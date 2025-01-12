5618556cf9c4c6477028f7616e90b87041d13fcc
Shivam Rajput <raj.shiviraj@gmail.com>
2025-01-12 14:16:52 +0530

    Shivi | update redis cache config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ publishing {
             from(components["java"])
             groupId = "com.robotutor"
             artifactId = "redis-starter"
-            version = "1.0.8"
+            version = "1.0.9"
 
             pom {
                 name.set("Redis Starter")
