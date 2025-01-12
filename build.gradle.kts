c78c902223f9f177782bedd7cec4d5268e683dc3
Shivam Rajput <raj.shiviraj@gmail.com>
2025-01-12 17:45:03 +0530

    Shivi | updated CacheService.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ publishing {
             from(components["java"])
             groupId = "com.robotutor"
             artifactId = "redis-starter"
-            version = "1.0.16"
+            version = "1.0.17"
 
             pom {
                 name.set("Redis Starter")
--- src/main/kotlin/com/robotutor/iot/service/CacheService.kt
+++ src/main/kotlin/com/robotutor/iot/service/CacheService.kt
@@ -11,32 +11,67 @@ import java.time.Duration
 
 @Service
 class CacheService(private val reactiveRedisTemplate: ReactiveRedisTemplate<String, String>) {
-    fun <T : Any> setValue(key: String, value: T, ttlInSeconds: Long = 600): Mono<T> {
-        return reactiveRedisTemplate.opsForValue()
-            .set(key, DefaultSerializer.serialize(value), Duration.ofSeconds(ttlInSeconds))
-            .map { value }
+    fun <T : Any> retrieve(key: String, ttlInSeconds: Long = 600, switchIfAbsent: () -> Mono<T>): Mono<T> {
+        return getValue<T>(key)
+            .switchIfEmpty(
+                switchIfAbsent()
+                    .flatMap { setValue(key, it, ttlInSeconds) }
+            )
     }
 
-    fun <T : Any> getValue(key: String, clazz: Class<T>): Mono<T> {
-        return reactiveRedisTemplate.opsForValue().get(key).map { DefaultSerializer.deserialize(it, clazz) }
+    fun <T : Any> retrieves(key: String, ttlInSeconds: Long = 600, switchIfAbsent: () -> Flux<T>): Flux<T> {
+        return getValues<T>(key)
+            .switchIfEmpty(
+                switchIfAbsent()
+                    .collectList()
+                    .flatMapMany { setValues(key, it, ttlInSeconds) }
+            )
     }
 
-    fun <T : Any> setValues(key: String, value: List<T>, ttlInSeconds: Long = 600): Flux<T> {
-        return reactiveRedisTemplate.opsForValue()
-            .set(key, DefaultSerializer.serialize(value), Duration.ofSeconds(ttlInSeconds))
-            .flatMapMany { createFlux(value) }
+    fun <T : Any> update(key: String, ttlInSeconds: Long = 600, switchIfAbsent: () -> Mono<T>): Mono<T> {
+        return evict(key)
+            .flatMap {
+                switchIfAbsent()
+                    .flatMap { setValue(key, it, ttlInSeconds) }
+            }
     }
 
-    fun <T : Any> getValues(key: String, clazz: Class<T>): Flux<T> {
-        val listType = object : TypeReference<List<T>>() {}.type
-        return reactiveRedisTemplate.opsForValue().get(key)
+    fun <T : Any> updates(key: String, ttlInSeconds: Long = 600, switchIfAbsent: () -> Flux<T>): Flux<T> {
+        return evict(key)
             .flatMapMany {
-                createFlux(DefaultSerializer.deserialize<List<T>>(it, listType))
+                switchIfAbsent()
+                    .collectList()
+                    .flatMapMany { setValues(key, it, ttlInSeconds) }
             }
     }
 
     fun evict(key: String): Mono<Boolean> {
         return reactiveRedisTemplate.opsForValue().delete(key)
+    }
+
+    private fun <T : Any> setValue(key: String, value: T, ttlInSeconds: Long = 600): Mono<T> {
+        return reactiveRedisTemplate.opsForValue()
+            .set(key, DefaultSerializer.serialize(value), Duration.ofSeconds(ttlInSeconds))
+            .map { value }
+    }
+
+    private fun <T : Any> getValue(key: String): Mono<T> {
+        val type = object : TypeReference<T>() {}.type
+        return reactiveRedisTemplate.opsForValue().get(key).map { DefaultSerializer.deserialize<T>(it, type) }
+    }
+
+    private fun <T : Any> setValues(key: String, value: List<T>, ttlInSeconds: Long = 600): Flux<T> {
+        return reactiveRedisTemplate.opsForValue()
+            .set(key, DefaultSerializer.serialize(value), Duration.ofSeconds(ttlInSeconds))
+            .flatMapMany { createFlux(value) }
+    }
+
+    private fun <T : Any> getValues(key: String): Flux<T> {
+        val listType = object : TypeReference<List<T>>() {}.type
+        return reactiveRedisTemplate.opsForValue().get(key)
+            .flatMapMany {
+                createFlux(DefaultSerializer.deserialize<List<T>>(it, listType))
+            }
     }
 }
 
