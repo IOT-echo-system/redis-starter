ecbf0dcde46ae885e22a59ba55d93f693a9e5b52
Shivam Rajput <raj.shiviraj@gmail.com>
2025-01-12 14:00:35 +0530

    Shivi | Add redis serialization.

--- src/main/kotlin/com/robotutor/iot/serializer/GsonRedisSerializer.kt
+++ src/main/kotlin/com/robotutor/iot/serializer/GsonRedisSerializer.kt
@@ -1,15 +1,26 @@
 package com.robotutor.iot.serializer
 
-import com.robotutor.loggingstarter.serializer.DefaultSerializer
+import com.robotutor.loggingstarter.serializer.ObjectMapperCache
 import org.springframework.data.redis.serializer.RedisSerializer
+import org.springframework.data.redis.serializer.SerializationException
 import java.nio.charset.StandardCharsets
 
-class GsonRedisSerializer<T : Any>(private val clazz: Class<T>) : RedisSerializer<T> {
-    override fun serialize(value: T?): ByteArray {
-        return DefaultSerializer.serialize(value).toByteArray(StandardCharsets.UTF_8)
+class GsonRedisSerializer<T> : RedisSerializer<T> {
+    override fun serialize(t: T?): ByteArray? {
+        return try {
+            t?.let { ObjectMapperCache.objectMapper.toJson(it).toByteArray(StandardCharsets.UTF_8) }
+        } catch (e: Exception) {
+            throw SerializationException("Error serializing object: $t", e)
+        }
     }
 
     override fun deserialize(bytes: ByteArray?): T? {
-        return bytes?.let { DefaultSerializer.deserialize(bytes.toString(StandardCharsets.UTF_8), clazz) }
+        return try {
+            bytes?.let {
+                ObjectMapperCache.objectMapper.fromJson(String(it, StandardCharsets.UTF_8), Any::class.java) as T
+            }
+        } catch (e: Exception) {
+            throw SerializationException("Error deserializing bytes: ${String(bytes ?: ByteArray(0))}", e)
+        }
     }
 }
